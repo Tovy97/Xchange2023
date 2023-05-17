17b9c2a8d53760e965ff553f760a1839919944ad
Marco Mantovani <marc.mantovani@goreply.io>
2023-05-17 09:44:19 +0200

    [MOD] deploy script

--- Ingestion/deploy.ps1
+++ Ingestion/deploy.ps1
@@ -1,20 +1,34 @@
-gcloud functions deploy ingest-data `
-    --project goreply-xchange2023-datastudio `
-    --region europe-west1 `
-    --no-allow-unauthenticated `
-    --entry-point ingest_data `
-    --gen2 `
-    --memory 512MiB `
-    --retry `
-    --run-service-account cloud-function-sa@goreply-xchange2023-datastudio.iam.gserviceaccount.com `
-    --service-account    cloud-function-sa@goreply-xchange2023-datastudio.iam.gserviceaccount.com `
-    --runtime python310 `
-    --serve-all-traffic-latest-revision `
-    --source . `
-    --timeout 540s `
-    --trigger-location europe-west1 `
-    --trigger-service-account eventarc-trigger-sa@goreply-xchange2023-datastudio.iam.gserviceaccount.com `
-    --max-instances 10 `
-    --min-instances 0 `
-    --trigger-event google.cloud.storage.object.v1.finalized `
-    --trigger-resource xchange-23
+$FUNCTION_NAME = "ingest-data"
+$ENTRY_POINT = "ingest_data"
+$PROJECT = "goreply-xchange2023-datastudio"
+$REGION = "europe-west1"
+$MEMORY = "512MiB"
+$RUNTIME = "python310"
+$TIMEOUT = "540s"
+$SOURCE = "."
+$MAX_INSTANCE = 10
+$MIN_INSTANCE = 0
+$BUCKET_NAME = "xchange-23"
+$FUNCTION_SA = "cloud-function-sa@goreply-xchange2023-datastudio.iam.gserviceaccount.com"
+$EVENTARC_SA = "eventarc-trigger-sa@goreply-xchange2023-datastudio.iam.gserviceaccount.com"
+
+gcloud functions deploy                     $FUNCTION_NAME `
+    --project                               $PROJECT `
+    --region                                $REGION `
+    --no-allow-unauthenticated              `
+    --entry-point                           $ENTRY_POINT `
+    --gen2                                  `
+    --memory                                $MEMORY `
+    --retry                                 `
+    --run-service-account                   $FUNCTION_SA `
+    --service-account                       $FUNCTION_SA `
+    --runtime                               $RUNTIME `
+    --serve-all-traffic-latest-revision     `
+    --source                                $SOURCE `
+    --timeout                               $TIMEOUT `
+    --trigger-location                      $REGION `
+    --trigger-service-account               $EVENTARC_SA `
+    --max-instances                         $MAX_INSTANCE `
+    --min-instances                         $MIN_INSTANCE `
+    --trigger-event                         google.cloud.storage.object.v1.finalized `
+    --trigger-resource                      $BUCKET_NAME
